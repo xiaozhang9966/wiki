10406fe8c4485ee7389c11c56e95a1566b4c4a6d
Nice233666 <791371161@qq.com>
2022-08-29 15:47:46 +0800

    8.29 新建Miki项目

---
+++ src/main/java/com/lrvinglm/miki/MikiApplication.java
@@ -0,0 +1,13 @@
+package com.lrvinglm.miki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikiApplication.class, args);
+    }
+
+}
