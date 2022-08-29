a729e24ce789a214b256a7b329423abf347f9283
‘Nice233666 <791371161@qq.com>
2022-08-29 20:53:24 +0800

    8.29 增加logback.xml配置文件,增加了日志log,在日志中添加了运行成功的语句

--- src/main/java/com/lrvinglm/miki/MikiApplication.java
+++ src/main/java/com/lrvinglm/miki/MikiApplication.java
@@ -1,14 +1,22 @@
 package com.lrvinglm.miki;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class MikiApplication {
-
+    private static final Logger LOG= LoggerFactory.getLogger(MikiApplication.class);
     public static void main(String[] args) {
-
-        SpringApplication.run(MikiApplication.class, args);
+        SpringApplication app=new SpringApplication(MikiApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
