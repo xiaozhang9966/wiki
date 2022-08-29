30b9625ce76874702358dacdcf80b1e00bded656
‘Nice233666 <791371161@qq.com>
2022-08-29 21:10:46 +0800

    8.29 增加启动图案

--- src/main/java/com/lrvinglm/miki/MikiApplication.java
+++ src/main/java/com/lrvinglm/miki/MikiApplication.java
@@ -11,10 +11,11 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MikiApplication {
+    //LOG作用LoggerFactory.getLogger可以在IDE控制台打印日志,便于开发,一般加在代码最上面
     private static final Logger LOG= LoggerFactory.getLogger(MikiApplication.class);
     public static void main(String[] args) {
         SpringApplication app=new SpringApplication(MikiApplication.class);
-        Environment env=app.run(args).getEnvironment();
+        Environment env=app.run(args).getEnvironment();//Environment 这个接口代表应用运行时的环境。它代表了两方面、一个是 profile 一个是 properties。
         LOG.info("启动成功！");
         LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
