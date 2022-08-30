af2db046c51d58e7b3d7b5f8e2c294a624c028e7
‘Nice233666 <791371161@qq.com>
2022-08-30 15:18:44 +0800

    8.30 添加了测试接口的http文件,并增加了类中自动去除无用包的设置

--- src/main/java/com/lrvinglm/miki/controller/TestControl.java
+++ src/main/java/com/lrvinglm/miki/controller/TestControl.java
@@ -1,7 +1,8 @@
 package com.lrvinglm.miki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //@Controller  返回页面 @RestController 是返回字符串的
 @RestController  //@ResponseBody用来返回字符串或JSON对象 大多是JSON对象
@@ -17,5 +18,10 @@ public class TestControl {
     @RequestMapping("/hello")  //访问的接口路径 表示这个接口支持所有的请求方式
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World"+name;
     }
 }
