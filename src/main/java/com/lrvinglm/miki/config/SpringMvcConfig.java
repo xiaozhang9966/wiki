099c1d98c83b9f91f4dc2e2c70247dee15a460df
‘Nice233666 <791371161@qq.com>
2022-09-03 17:02:19 +0800

    9.3 使用AOP，打印接口耗时,请求参数和返回参数 取代哦过滤器,拦截器

--- src/main/java/com/lrvinglm/miki/config/SpringMvcConfig.java
+++ src/main/java/com/lrvinglm/miki/config/SpringMvcConfig.java
@@ -1,23 +1,19 @@
 package com.lrvinglm.miki.config;
 
 
-import com.lrvinglm.miki.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    LogInterceptor logInterceptor;
+//    @Resource
+//    LogInterceptor logInterceptor;
 
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                //针对所有请求             过滤掉这些请求
-                .addPathPatterns("/**").excludePathPatterns("/login");
-
-    }
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                //针对所有请求             过滤掉这些请求
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//
+//    }
 }
--- src/main/java/com/lrvinglm/miki/filter/LogFilter.java
+++ src/main/java/com/lrvinglm/miki/filter/LogFilter.java
@@ -1,33 +1,33 @@
- package com.lrvinglm.miki.filter;
-
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.stereotype.Component;
-
- import javax.servlet.*;
- import javax.servlet.http.HttpServletRequest;
- import java.io.IOException;
-
- @Component //识别为组件
- public class LogFilter implements Filter {
-
-     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
-
-     }
-
-     @Override
-     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-         // 打印请求信息
-         HttpServletRequest request = (HttpServletRequest) servletRequest;
-         LOG.info("------------- LogFilter 开始 -------------");
-         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());//请求地址和请求方式
-         LOG.info("远程地址: {}", request.getRemoteAddr());//返回请求发送的地址
-
-         long startTime = System.currentTimeMillis();
-         filterChain.doFilter(servletRequest, servletResponse);
-         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+// package com.lrvinglm.miki.filter;
+//
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
+//
+// import javax.servlet.*;
+// import javax.servlet.http.HttpServletRequest;
+// import java.io.IOException;
+//
+// @Component //识别为组件
+// public class LogFilter implements Filter {
+//
+//     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//     @Override
+//     public void init(FilterConfig filterConfig) throws ServletException {
+//
+//     }
+//
+//     @Override
+//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//         // 打印请求信息
+//         HttpServletRequest request = (HttpServletRequest) servletRequest;
+//         LOG.info("------------- LogFilter 开始 -------------");
+//         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());//请求地址和请求方式
+//         LOG.info("远程地址: {}", request.getRemoteAddr());//返回请求发送的地址
+//
+//         long startTime = System.currentTimeMillis();
+//         filterChain.doFilter(servletRequest, servletResponse);
+//         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//     }
+// }
