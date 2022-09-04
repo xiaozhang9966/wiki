185790368e4af9f9fdc5d49c35f9fbcfc215c802
‘Nice233666 <791371161@qq.com>
2022-09-04 17:26:34 +0800

    9.4 集成PageHelper插件 将mapper日志级别改为trace,打印所有的sql日志

--- src/main/java/com/lrvinglm/miki/service/EbookService.java
+++ src/main/java/com/lrvinglm/miki/service/EbookService.java
@@ -1,12 +1,17 @@
 package com.lrvinglm.miki.service;
 
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.lrvinglm.miki.aspect.LogAspect;
 import com.lrvinglm.miki.domain.Ebook;
 import com.lrvinglm.miki.domain.EbookExample;
 import com.lrvinglm.miki.mapper.EbookMapper;
 import com.lrvinglm.miki.req.EbookReq;
 import com.lrvinglm.miki.resp.EbookResp;
 import com.lrvinglm.miki.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -18,7 +23,12 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
     public List<EbookResp> list(EbookReq req){
+
+
+
         //domain下的example mybaits自动生成了很多方法
         EbookExample ebookExample = new EbookExample();
         //当作where语句
@@ -26,9 +36,12 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){//不为空才执行
             criteria.andNameLike("%"+req.getName()+"%"); //模糊查询的条件
         }
-
+        PageHelper.startPage(1,2);//只会分页最近的需要查询的sql，当页面多条sql时 把分页和sql放一起
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);//查询到所有的Ebook实体
 
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        LOG.info("页数:{}",pageInfo.getPages());
+        LOG.info("行数：{}",pageInfo.getTotal());
 //        List<EbookResp> respList=new ArrayList<>();
         //遍历所有的Ebook属性给EbookResp 并过滤掉不需要返回的属性
 //        for (Ebook e:ebookList) {
