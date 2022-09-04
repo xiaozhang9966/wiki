e5b77285b6f66935a282267065170703536a8508
‘Nice233666 <791371161@qq.com>
2022-09-04 14:56:17 +0800

    9.4 修改电子管理页面

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,6 +2,12 @@
   <div class="admin-ebook">
     <h1>电子书管理</h1>
   </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+
+    </a-layout-content>
+  </a-layout>>
 </template>
 
 <script>
