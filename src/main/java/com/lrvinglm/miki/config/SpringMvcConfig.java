95bafef7e4b08803d5d9f2e59d041e53719d4558
‘Nice233666 <791371161@qq.com>
2022-09-04 14:24:21 +0800

    9.4 修改为小写

--- src/main/java/com/lrvinglm/miki/config/SpringMvcConfig.java
+++ src/main/java/com/lrvinglm/miki/config/SpringMvcConfig.java
@@ -1,10 +1,9 @@
 package com.lrvinglm.miki.config;
 
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
 //    @Resource
