6e04311f4a0e944d4e8b9783881d11aff02e82e7
小张9966 <2387479217@qq.com>
2022-09-11 20:01:07 +0800

    9.11 分类表设计与持久层代码生成 基本的无限级树形结构设计：id+parent

--- doc/all.sql
+++ doc/all.sql
@@ -40,3 +40,27 @@ insert into `ebook` (id,name,description) value (2,'Vue 入门教程','高成本
 insert into `ebook` (id,name,description) value (3,'Apex入门教程','三天上猎杀');
 
 insert into `ebook` (id,name,description) value (4,'CSGO低端局无脑套路','爷们要战斗~爷们要战斗~');
+
+
+#分类
+drop table if exists `category`;
+create table `category`(
+                           `id` bigint not null comment  'id',
+                           `parent` bigint not null  default  0 comment '父id',
+                           `name` varchar(50) not null comment '名称',
+                           `sort` int comment '顺序',
+                           primary key (`id`)
+)engine=innodb default charset =utf8mb4 comment='分类';
+
+insert into `category` (id, parent, name, sort) values(100,000,'前端开发',100);
+insert into `category` (id, parent, name, sort) values(101,100,'vue',101);
+insert into `category` (id, parent, name, sort) values(102,100,'csss',102);
+insert into `category` (id, parent, name, sort) values(200,000,'Html',200);
+insert into `category` (id, parent, name, sort) values(201,200,'java',201);
+insert into `category` (id, parent, name, sort) values(202,200,'python',202);
+insert into `category` (id, parent, name, sort) values(300,000,'数据库',300);
+insert into `category` (id, parent, name, sort) values(301,300,'mysql',301);
+insert into `category` (id, parent, name, sort) values(302,300,'服务器',302);
+insert into `category` (id, parent, name, sort) values(400,000,'开发工具',400);
+insert into `category` (id, parent, name, sort) values(401,400,'其他',401);
+insert into `category` (id, parent, name, sort) values(402,400,'集成开发',402);
