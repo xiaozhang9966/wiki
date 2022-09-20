2e1ea86f3572f0dd0de51a861f1e344f79bb7306
小张9966 <2387479217@qq.com>
2022-09-20 16:27:21 +0800

    9.20 增加读取文档内容功能 编辑和新增的时候，先清空富文本框 出现bug（系统异常请联系管理员）

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -276,8 +276,8 @@ export default defineComponent({
         modalLoading.value = false;
         const data = response.data;  //commonResp
         if(data.success){
-          modalVisible.value = false;
-
+          //modalVisible.value = false;
+          message.success("保存成功！")
           //重新加载列表
           handleQuery();
         }else {
@@ -304,7 +304,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = ( record:any ) =>{
-
+      //先清空富文本框
+      editor.txt.html("");
       modalVisible .value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -321,7 +322,8 @@ export default defineComponent({
      * 添加
      */
     const add = () =>{
-
+      //先清空富文本框
+      editor.txt.html("");
       modalVisible .value = true;
       doc.value={
         ebookId: route.query.ebookId
